12ac1b2455ed32852f38ff1b1c04080d2a97063e
Davi Pereira <davi.p.m94@gmail.com>
2025-07-01 18:38:21 +0900

    Initialize React + Vite project template with Tailwind CSS support.

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import About from "./components/about.jsx";
+
+export default function App() {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <About />
+    </main>
+  )
+}
+
---
+++ src/components/about.jsx
@@ -0,0 +1,56 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
+
+export default function About() {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: false,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: 0,
+    });
+  });
+
+  return (
+    <div className="min-h-screen w-screen" id="about">
+      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          Welcome to Zentry
+        </p>
+
+        <div />
+
+        <div className="about-subtext">
+          <p>The Game of Games begins—your life, now an epic MMORPG</p>
+          <p className="text-gray-500">
+            Zentry unites every player from countless games and platforms, both
+            digital and physical, into a unified Play Economy
+          </p>
+        </div>
+      </div>
+
+      <div className="w-screen h-dvh" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="/img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
