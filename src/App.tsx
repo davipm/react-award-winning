7a773896aff35bf27072ef15b1e6629d5f0d1e59
Davi Pereira <davi.p.m94@gmail.com>
2025-07-03 18:15:09 +0900

    Add reusable `Button` component and integrate it into `Story` section with updated GSAP interactions.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import About from "./components/about.tsx";
 import Features from "./components/features.tsx";
+import Story from "./components/story.tsx";
 
 export default function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <About />
       <Features />
+      <Story />
     </main>
   );
 }
--- src/components/story.tsx
+++ src/components/story.tsx
@@ -3,9 +3,10 @@ import { useRef } from "react";
 
 import type { MouseEventProps } from "./features.tsx";
 import AnimatedTitle from "./animated-title.tsx";
+import Button from "./button.tsx";
 
 export default function Story() {
-  const frameRef = useRef<HTMLDivElement>(null);
+  const frameRef = useRef<HTMLImageElement>(null);
 
   const handleMouseMove = (event: MouseEventProps) => {
     const { clientX, clientY } = event;
@@ -32,9 +33,92 @@ export default function Story() {
     });
   };
 
+  const handleMouseLeave = () => {
+    const element = frameRef.current;
+
+    if (element) {
+      gsap.to(element, {
+        duration: 0.3,
+        rotateX: 0,
+        rotateY: 0,
+        ease: "power1.inOut",
+      });
+    }
+  };
+
   return (
-    <div id="story" className="min-h-dvh wscreen bg-black text-blue-50">
-      <div></div>
+    <div id="story" className="min-h-dvh w-screen bg-black text-blue-50">
+      <div className="flex size-full flex-col items-center py-10 pb-24">
+        <p className="font-general text-sm uppercase md:text-[10px]">
+          the multiversal ip world
+        </p>
+
+        <div className="relative size-full">
+          <AnimatedTitle
+            title="the st<b>o</b>ry of <br /> a hidden real<b>m</b>"
+            containerClass="mt-5 pointer-events-none mix-blend-difference relative z-10"
+          />
+
+          <div className="story-img-container">
+            <div className="story-img-mask">
+              <div className="story-img-content">
+                <img
+                  ref={frameRef}
+                  src="/img/entrance.webp"
+                  alt="entrance.webp"
+                  className="object-contain"
+                  onMouseMove={handleMouseMove}
+                  onMouseLeave={handleMouseLeave}
+                  onMouseUp={handleMouseLeave}
+                  onMouseEnter={handleMouseLeave}
+                />
+              </div>
+            </div>
+
+            <svg
+              className="invisible absolute size-0"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <defs>
+                <filter id="flt_tag">
+                  <feGaussianBlur
+                    in="SourceGraphic"
+                    stdDeviation="8"
+                    result="blur"
+                  />
+                  <feColorMatrix
+                    in="blur"
+                    mode="matrix"
+                    values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 19 -9"
+                    result="flt_tag"
+                  />
+                  <feComposite
+                    in="SourceGraphic"
+                    in2="flt_tag"
+                    operator="atop"
+                  />
+                </filter>
+              </defs>
+            </svg>
+          </div>
+        </div>
+
+        <div className="-mt-80 flex w-full justify-center md:-mt-64 md:me-44 md:justify-end">
+          <div className="flex h-full w-fit flex-col items-center md:items-start">
+            <p className="mt-3 max-w-sm text-center font-circular-web text-violet-50 md:text-start">
+              Where realms converge, lies Zentry and the boundless pillar.
+              Discover its secrets and shape your fate amidst infinite
+              opportunities.
+            </p>
+
+            <Button
+              id="realm-btn"
+              title="discover prologue"
+              containerClass="mt"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
