17625b225e4bd53020abb53711064559d5b5f12c
Davi Pereira <davi.p.m94@gmail.com>
2025-07-07 15:31:30 +0900

    Add `Hero` and `VideoPreview` components with GSAP animations; integrate `Hero` into `App`.

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import Story from "./components/story.tsx";
 import Contact from "./components/contact.tsx";
 import Footer from "./components/footer.tsx";
 import NavBar from "./components/nav-bar.tsx";
+import Hero from "./components/hero.tsx";
 
 export default function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
       <NavBar />
+      <Hero />
       <About />
       <Features />
       <Story />
---
+++ src/components/video-preview.tsx
@@ -0,0 +1,82 @@
+import gsap from "gsap";
+import { useEffect, useRef, useState } from "react";
+
+export default function VideoPreview({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isHovering, setIsHovering] = useState(false);
+
+  const sectionRef = useRef(null);
+  const contentRef = useRef(null);
+
+  const handleMouseMove = ({
+    clientX,
+    clientY,
+    currentTarget,
+  }: React.MouseEvent) => {
+    const rect = currentTarget.getBoundingClientRect();
+
+    const xOffset = clientX - (rect.left + rect.width / 2);
+    const yOffset = clientY - (rect.top + rect.height / 2);
+
+    if (isHovering) {
+      gsap.to(sectionRef.current, {
+        x: xOffset,
+        y: yOffset,
+        rotateX: xOffset / 2,
+        rotateY: -yOffset / 2,
+        transformPerspective: 500,
+        duration: 1,
+        ease: "power1.out",
+      });
+
+      gsap.to(contentRef.current, {
+        x: -xOffset,
+        y: -yOffset,
+        duration: 1,
+        ease: "power1.out",
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (!isHovering) {
+      gsap.to(sectionRef.current, {
+        x: 0,
+        y: 0,
+        rotationY: 0,
+        rotationX: 0,
+        duration: 1,
+        ease: "power1.out",
+      });
+
+      gsap.to(contentRef.current, {
+        x: 0,
+        y: 0,
+        duration: 1,
+        ease: "power1.out",
+      });
+    }
+  }, []);
+
+  return (
+    <section
+      ref={sectionRef}
+      onMouseMove={handleMouseMove}
+      onMouseEnter={() => setIsHovering(true)}
+      onMouseLeave={() => setIsHovering(false)}
+      className="absolute z-50 size-full overflow-hidden rounded-lg"
+      style={{ perspective: "500px" }}
+    >
+      <div
+        ref={contentRef}
+        className="origin-center rounded-lg"
+        style={{ transformStyle: "preserve-3d" }}
+      >
+        {children}
+      </div>
+    </section>
+  );
+}
