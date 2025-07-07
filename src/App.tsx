436d0b25ebd7323e611ce5d0fba5ec72721c3e05
Davi Pereira <davi.p.m94@gmail.com>
2025-07-07 13:36:25 +0900

    Add `NavBar` component with dynamic visibility and GSAP animations; integrate it into `App`.

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Features from "./components/features.tsx";
 import Story from "./components/story.tsx";
 import Contact from "./components/contact.tsx";
 import Footer from "./components/footer.tsx";
+import NavBar from "./components/nav-bar.tsx";
 
 export default function App() {
   return (
     <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <NavBar />
       <About />
       <Features />
       <Story />
