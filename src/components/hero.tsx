7e704ff4540b98e2a49d35da57cf3c4cea8226e8
Davi Pereira <davi.p.m94@gmail.com>
2025-07-07 15:45:10 +0900

    Update `Hero` component: adjust container height to use `h-dvh` and add hover cursor to `mask-clip-path` for better interactivity.

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -95,10 +95,10 @@ export default function Hero() {
 
       <div
         id="video-frame"
-        className="relative z-10 w-screen overflow-hidden rounded-lg bg-violet-75"
+        className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-violet-75"
       >
         <div>
-          <div className="mask-clip-path absolute-center absolute z-50 size-64">
+          <div className="mask-clip-path absolute-center absolute z-50 size-64 hover:cursor-pointer">
             <VideoPreview>
               <div
                 className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100"
