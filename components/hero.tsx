f44fa76be9e22c74f6fcf8841586776ea00f9c7f
Davi Pereira <davi.p.m94@gmail.com>
2026-09-20 21:03:14 +0900

    fix bg video

--- components/hero.tsx
+++ components/hero.tsx
@@ -75,7 +75,6 @@ export function Hero() {
             ref={mainVideoRef}
             id={VIDEO_IDS.MAIN}
             src={mainVideoSrc}
-            poster="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR42mNkYPhfDwAChwGA60e6kgAAAABJRU5ErkJggg=="
             loop
             muted
             playsInline
